8f248b51df44782b023c769cd710464c24ecd3f8
Max Kneisl <mkneisl@student.42heilbronn.de>
2024-11-07 12:12:36 +0100

    Added ft_list_push_front - untested

--- src/bonus/ft_list_push_front.asm
+++ src/bonus/ft_list_push_front.asm
@@ -2,4 +2,21 @@
 %include "src/bonus/list_macros.asm"
 
     global ft_list_push_front
-    
+
+    extern ft_list_new
+    section .text
+ft_list_push_front:
+    push rdi
+    mov rdi, rsi
+    dycall ft_list_new
+    cmp rax, 0
+    pop rdi
+    je end
+    cmp [rdi], 0
+    je set_start_node
+    mov rdx, [rdi]
+    mov [rax + s_list.next], rdx
+set_start_node:
+    mov [rdi], rax
+end:
+    ret
