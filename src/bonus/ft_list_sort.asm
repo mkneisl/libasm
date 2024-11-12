0984284ff806527585b0f2f6fcc1615029a8f91d
Max Kneisl <mkneisl@student.42heilbronn.de>
2024-11-12 12:25:25 +0100

    Removed stupidness

--- src/bonus/ft_list_sort.asm
+++ src/bonus/ft_list_sort.asm
@@ -2,9 +2,6 @@
 %include "src/bonus/list_macros.asm"
 
 	global ft_list_sort
-	extern ft_list_push_front
-	extern ft_list_new
-	extern free
 
 	struc stack
 		.sortedList:	resq 1
@@ -25,25 +22,23 @@ ft_list_sort:
 	mov [rsp + stack.callback], rsi
 	lea rdi, [rsp + stack.sortedList]
 	mov QWORD [rdi], 0
-	mov rsi, [rax + s_list.data]		; Pushing first element of list into sorted list
-	mov [rsp + stack.currentNode], rax
-	dycall ft_list_push_front
-	mov rsi, [rsp + stack.currentNode]
+	mov rsi, rax						; Pushing first element of list into sorted list
+	mov rax, [rax + s_list.next]
+	mov rcx, [rdi]
+	mov [rsi + s_list.next], rcx
+	mov [rdi], rsi
+	mov rsi, rax
 loop_unsorted:
-	mov rsi, [rsi + s_list.next]		; Get next element of unsorted list
 	cmp rsi, 0
 	je end
-	mov [rsp + stack.currentNode], rsi
+	mov rax, [rsi + s_list.next]		; Get next element of unsorted list
+	mov [rsp + stack.currentNode], rax
 	lea rdi, [rsp + stack.sortedList]
 	mov rdx, [rsp + stack.callback]
 	call insert_sorted					; Insert element sorted
 	mov rsi, [rsp + stack.currentNode]
-	; mov rsi, [rsi + s_list.next]
 	jmp loop_unsorted
 end:
-	mov rdi, [rsp + stack.unsortedList]
-	mov rdi, [rdi]
-	call deallocate_list
 	mov rsi, [rsp + stack.unsortedList]
 	mov rdi, [rsp + stack.sortedList]
 	mov [rsi], rdi
@@ -59,9 +54,6 @@ insert_sorted:
 	mov r13, rsi	; r13 -> node to insert
 	mov r14, rdx	; r14 -> cmp callback
 	mov r15, [rdi]	; r15 -> current node
-	mov rdi, [r13 + s_list.data]
-	dycall ft_list_new
-	mov r13, rax
 insertion_loop:
 	cmp r15, 0
 	je insert_sorted_end
@@ -72,7 +64,7 @@ insertion_loop:
 	jle insert_node
 	lea r12, [r15 + s_list.next]
 	mov r15, [r15 + s_list.next]
-	jmp insertion_loop 				; if greater - get next node else insert
+	jmp insertion_loop				; if greater - get next node else insert
 insert_node:
 	mov [r13 + s_list.next], r15
 	mov [r12], r13
@@ -81,16 +73,4 @@ insert_sorted_end:
 	pop r14
 	pop r13
 	pop r12
-	ret
-
-deallocate_list:
-	mov rcx, [rdi + s_list.next]
-	push rcx
-	dycall free
-	pop rcx
-	cmp rcx, 0
-	je deallocate_list_end
-	mov rdi, rcx
-	jmp deallocate_list
-deallocate_list_end:
 	ret
